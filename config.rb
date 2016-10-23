18e6bdb3971e60eb810c8e58596372d542dca3ec
Eric Amundson <eric@ivycat.com>
2016-10-23 16:47:56 -0700

    moving assets to assets folder

--- config.rb
+++
@@ -1,7 +0,0 @@
-css_dir = 'assets/css'
-fonts_dir = 'assets/fonts'
-images_dir = 'assets/images'
-javascripts_dir = 'assets/js'
-sass_dir = 'assets/sass'
-output_style = :nested
-relative_assets = true
