28a659a167d62793a6fa41a953b807ac74cdad1e
Eric Amundson <eric@ivycat.com>
2016-10-24 01:37:10 -0700

    more refactoring and hookup

--- functions.php
+++ functions.php
@@ -104,9 +104,9 @@ add_action( 'widgets_init', 'bitsy_widgets_init' );
 function bitsy_scripts() {
 	wp_enqueue_style( 'bitsy-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'bitsy-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'bitsy-navigation', get_template_directory_uri() . '/assets/js/source/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'bitsy-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'bitsy-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/source/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
