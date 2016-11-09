3ebe05528aecc37af2f11cf0c2f6ede1ac3c23bd
Eric Amundson <eric@ivycat.com>
2016-11-08 19:22:06 -0800

    change nav class and ids for main nav

--- header.php
+++ header.php
@@ -40,8 +40,8 @@
 	</header><!-- #header -->
 
 	<nav id="nav" class="main-navigation" role="navigation">
+		<?php wp_nav_menu( array( 'theme_location' => 'primary','menu_id' => 'primary-menu','menu_class' => 'links','container' => 'false','depth' => '1' ) ); ?>
 		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bitsy' ); ?></button>
-		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 	</nav><!-- #site-navigation -->
 	
 	<section id="banner">
