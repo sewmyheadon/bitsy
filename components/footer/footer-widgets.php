df383ed38e89a23e318ca9be430ec56893ca5f03
Eric Amundson <eric@ivycat.com>
2018-01-14 18:28:45 -0800

    Update vertical rhythm based on 20px root font size, 30px line-height

--- components/footer/footer-widgets.php
+++ components/footer/footer-widgets.php
@@ -1,6 +1,6 @@
 <div class="row double">
 	<div class="6u 12u$(medium)">
-		<h2>Aliquam Interdum</h2>
+		<h3>Aliquam Interdum</h3>
 		<p>Ornare interdum nascetur enim lobortis nunc amet placerat pellentesque nascetur in adipiscing. Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan. Justo aliquet massa adipiscing cubilia eu accumsan id. Arcu accumsan faucibus vis ultricies adipiscing ornare ut. Mi accumsan. Justo aliquet massa adipiscing cubilia eu accumsan id lorem ipsum dolor.</p>
 	</div>
 	<div class="3u 6u(medium) 12u$(small)">
