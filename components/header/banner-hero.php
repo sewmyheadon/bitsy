4824f0ab63ed8f0aaf8e9877c059461c5b1e3160
Eric Amundson <eric@ivycat.com>
2018-04-13 15:05:21 -0700

    Add custom header

--- components/header/banner-hero.php
+++ components/header/banner-hero.php
@@ -1,9 +1,10 @@
-	<section id="banner">
-		<h2>Hi, I'm Bitsy</h2>
-		<p>A super simple starting point for projects based on <a href="http://skel.io">Skel</a> &amp; <a href="http://underscores.me">_s</a>.</p>
-		<ul class="actions">
-			<li><a href="#content" class="button special">Content</a></li>
-			<li><a href="#elements" class="button">Elements</a></li>
-			<li><a href="#grid" class="button alt">Grid System</a></li>
-		</ul>
-	</section>
+<section id="banner">
+    <h2>Hi, I'm Bitsy</h2>
+    <p>A super simple starting point for projects based on <a href="http://skel.io">Skel</a> &amp; <a
+                href="http://underscores.me">_s</a>.</p>
+    <ul class="actions">
+        <li><a href="#content" class="button special">Content</a></li>
+        <li><a href="#elements" class="button">Elements</a></li>
+        <li><a href="#grid" class="button alt">Grid System</a></li>
+    </ul>
+</section>
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -4,7 +4,7 @@
  *
  * You can add an optional custom header image to header.php like so ...
  *
-	<?php the_header_image_tag(); ?>
+ * <?php the_header_image_tag(); ?>
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
@@ -18,14 +18,17 @@
  */
 function bitsy_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'bitsy_custom_header_args', array(
-		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
-		'flex-height'            => true,
-		'wp-head-callback'       => 'bitsy_header_style',
+		'default-image'      => get_template_directory_uri() . 'assets/images/fedora_smoke.jpg',
+		'header-text'        => true,
+		'default-text-color' => '000',
+		'flex-width'         => true,
+		'width'              => 2000,
+		'flex-height'        => true,
+		'height'             => 420,
+		'wp-head-callback'   => 'bitsy_header_style',
 	) ) );
 }
+
 add_action( 'after_setup_theme', 'bitsy_custom_header_setup' );
 
 if ( ! function_exists( 'bitsy_header_style' ) ) {
