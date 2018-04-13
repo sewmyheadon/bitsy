91077e19805df02ca1c7ca09e1d4ac0b1c6cdd76
Eric Amundson <eric@ivycat.com>
2018-04-13 12:02:18 -0700

    Adjust wrappers

--- wrapper-blank.php
+++ wrapper-blank.php
@@ -13,12 +13,10 @@
  */
 
 get_header( bitsy_template_base() ); ?>
-    <div class="row">
-		<?php bitsy_left_sidebar(); ?>
-        <div id="primary" class="content-area <?php bitsy_content_classes(); ?>" role="main">
-			<?php include bitsy_template_path(); ?>
-        </div><!-- #primary -->
-		<?php bitsy_right_sidebar(); ?>
-    </div>
-<?php //get_sidebar( bitsy_template_base() ); Might use as wide footer widget area
+
+    <main class="site-main" id="main">
+		<?php include bitsy_template_path(); ?>
+    </main><!-- #main -->
+
+<?php
 get_footer( bitsy_template_base() );
